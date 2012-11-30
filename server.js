658788b2ed4e3e2068ed616c5a38977b652fedc9
Tiago Rodrigues <tmcrodrigues@gmail.com>
2012-11-30 16:07:21 +0000

    loads of stuff

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    faye = require('faye');
+
+
+var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+
+// Handle non-Bayeux requests
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello, non-Bayeux request');
+  response.end();
+});
+
+bayeux.attach(server);
+
+
+bayeux.getClient().subscribe('/play', function(message) {
+});
+
+server.listen(1338);
